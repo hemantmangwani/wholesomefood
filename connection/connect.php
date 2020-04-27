fd3993ace9a8f241e2b9626e803c1bb243a182e5
Hemant Kumar Mangwani <hemantmangwani99@gmail.com>
2020-04-27 23:31:13 +0530

    Add files via upload

---
+++ connection/connect.php
@@ -0,0 +1,17 @@
+<?php
+
+//main connection file for both admin & front end
+$servername = "hemant99.mysql.database.azure.com"; //server
+$username = "hemant99@hemant99"; //username
+
+$password = "Hemant@123"; //password
+$dbname = "online_rest";  //database
+
+// Create connection
+$db = mysqli_connect($servername, $username, $password, $dbname); // connecting 
+// Check connection
+if (!$db) {       //checking connection to DB	
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
